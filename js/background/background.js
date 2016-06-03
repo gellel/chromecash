85fc852cde05b50eaf4f6858d1eaaedeb8c419c2
Lindsay Gelle <lindsay.gelle@gmail.com>
2016-06-03 14:42:48 +1000

    General rework to extension architecture. Relies more on background.js for better data management.

--- js/background/background.js
+++ js/background/background.js
@@ -1,8 +1,80 @@
-let t = "test";
+/**
+	* global instance of countries__data to be shared
+**/
 
+let countries__data = undefined;
+
+function fetchCountryJSON (callback) {
+
+	/**
+		* retrieve local instance of countries data
+		* if data does not exist locally, fetch from resources countries.json
+		* set and store for future instances
+		*
+		* @param function 	acts as callback response handler for shared data to content.js
+	**/
+
+	countries__data = countries__data || localStorage.getItem("countries__json") || undefined;
+
+	if (!countries__data) {
+
+		let countries__path = chrome.extension.getURL("js/json/countries/countries.json");
+
+		let xhr = new XMLHttpRequest();
+
+		xhr.open("GET", countries__path, true);
+		
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				countries__data = (xhr.status === 200) ? xhr.responseText : undefined;
+
+				if (countries__data) {
+				
+					localStorage.setItem("countries__json", countries__data);
+				}
+
+				callback(JSON.parse(countries__data));
+			}
+		};
+
+		xhr.send();
+
+	}
+	else {
+
+		if (typeof countries__data === "string") {
+			countries__data = JSON.parse(countries__data)
+		}
+
+		callback(countries__data);
+
+	};
+	
+};
+	
+/**
+	* general cross script service worker accepts messages from content scripts
+	* handles supplied request relative to the outcome desired such as requesting data through a function
+**/
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  
-	sendResponse({ t: t });
+
+	if (request) {
+
+		if (request.message_type === "function_request") {
+
+			if (!request.function_name) {
+				sendResponse({ meta: {status: { msg: "missing function name"}} });
+			}
+			else {
+				window[request.function_name](function (returned__data) {
+				
+					sendResponse({ c: chrome, meta: {status: { msg: "success"}}, data: returned__data });
+
+				});
+			}
+		}
+
+	}
 
 });
--- js/content/content.js
+++ js/content/content.js
@@ -5,60 +5,19 @@
 
 	!function (callback) {
 
-		/**
-			* retrieve local instance of countries data
-			* if data does not exist locally, fetch from resources countries.json
-			* set and store for future instances
-		**/
-		
-		let countries__data = localStorage.getItem("countries__json");
-
-
-		if (!countries__data) {
-
-			let countries__path = chrome.extension.getURL("js/json/countries/countries.json");
-
-			let xhr = new XMLHttpRequest();
-
-			xhr.open("GET", countries__path, true);
-			
-			xhr.onreadystatechange = function () {
-				if (xhr.readyState === 4) {
-					countries__data = (xhr.status === 200) ? xhr.responseText : undefined;
-
-					if (countries__data) {
-					
-						localStorage.setItem("countries__json", countries__data);
-					}
-
-					callback(countries__data);
-				}
-			};
-
-			xhr.send();
-
-		}
-		else {
-
-			callback(countries__data);
-
-		}
-
+		callback();
 
 	}(init);
 
 
-	function init (countries__data) {
+	function init () {
 
-		countries__data = JSON.parse(countries__data);
-
-		chrome.runtime.sendMessage({a: "test"}, function (a) {
+		
+		chrome.runtime.sendMessage({message_type: "function_request", function_name: "fetchCountryJSON"}, function (a) {
 
 			console.log(a);
 
 		})
-
-		//localStorage.removeItem("countries__json");
 
 	};
 
