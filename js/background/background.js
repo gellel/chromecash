1a57ced170b09a2e76a9f9d511f93a61d31a7c31
gellel <lindsay.gelle@gmail.com>
2016-06-09 16:10:38 +1000

    Further reworking of the initialisation mechanism

--- js/background/background.js
+++ js/background/background.js
@@ -1,61 +1,36 @@
 this.countries__data = undefined;
 
-this.fetchJSON = function (callback) {
-	if (!window.countries__data || window.countries__data === null) {
-		if (localStorage.getItem("countries__json") !== null) {
-			window.countries__data = JSON.parse(localStorage.getItem("countries__json"));
-
-			fetchJSON(callback);
-		}
-		else {
-			let xhr = new XMLHttpRequest();
-				xhr.addEventListener("readystatechange", function () {
-					if (xhr.readyState === 4) {
-						if (xhr.status === 200) {
-							localStorage.setItem("countries__json", xhr.responseText);
-							window.countries__data = JSON.parse(xhr.responseText);
-
-							fetchJSON(callback);
-						}
-					}
-				}, false);
-				xhr.open("GET", chrome.extension.getURL("js/json/countries/countries.json"), false);
-				xhr.send();
-		};
-	}
-	else {
-		callback(window.countries__data);
-	};
-};
-
-/*
-	* awaits tabsOnUpdate to complete before operating
-	* declare the eventListener for Messages to allow flexible requests for functions
-*/
-this.init = function () {
-	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-		if (typeof request === "object") {
-			if (request.message_type) {
-				if (request.message_type === "function_request") {
-					if (window[request.function_name]) {
-						window[request.function_name](function (returnedData) {		
-							sendResponse({c: chrome, s: sender, d: returnedData});
-						});
-					}
+this.l = function (callback) {
+	if (!countries__data) {
+		let url = chrome.extension.getURL("js/json/countries/countries.json");
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState === 4) {
+				if (xhr.status === 200) {	
+					countries__data = JSON.parse(xhr.responseText);
+					callback(countries__data);
 				}
 			}
-		}
-	});
-};
+		};
+		xhr.open("GET", url, false);
+		xhr.send();
+	}
+	else {
+		callback(countries__data);
+	}
+}
 
 /**
-	* primary initaliser
-	* asks tab[id] to initalise content functions
+	* general handler for background script
+	* operates via checking if the selected tab is ready (all content finished loading)
+	* executes ajax request for countries.json 
 **/
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	if (changeInfo.status === "complete") {
-		chrome.tabs.sendMessage(tabId, {meta: {status: "tab_ready", tabId: tabId}}, function () {
-			window.init();
+chrome.tabs.onUpdated.addListener(function (tabId, changeInformation, tab) {
+	if (changeInformation.status === "complete") {
+		l(function (r) {
+			chrome.tabs.sendMessage(tabId, {tab: tab, meta: {status: "ready"}, r: r}, function () {
+				if (!countries__data) countries__data = r;
+			});
 		});
-	}
+	} 
 });
--- js/content/content.js
+++ js/content/content.js
@@ -1,22 +1,22 @@
-/**
-	* retrieve JSON data from background.js
-	* hypothetically cached in background.js memory
-**/
-this.init = function () {
-	let countries__data = chrome.runtime.sendMessage({message_type: "function_request", function_name: "fetchJSON"}, function (response) {
-			console.log(response);
-	});
+this.init = function (request, callback) {
+	console.log(request);
 };
 
 /**
-	* activated from background.js
-	* unique response to tab content.js is operating on
+	* general message response handler between content.js and background.js
+	* primary operation to intilise content scripts after background scripts are set and tabs are ready
 **/
 chrome.runtime.onMessage.addListener(function (request, sender, sendMessage) {
 	if (typeof request === "object") {
-		if (request.meta) {
-			if (request.meta.status === "tab_ready") {
-				window.init();
+		if (Object.keys(request).length) {
+			if (request.meta) {
+				/**
+					* anticipates status to indicate first operation of script
+					* relies on background.js initialisation
+				**/
+				if (request.meta.status && request.meta.status === "ready") {
+					window.init(request);
+				}
 			}
 		}
 	}
