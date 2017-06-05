2f6bf9f337006d6d9d915eb3f7cc532e1aeefeea
gellel <lindsay.gelle@gmail.com>
2017-06-05 18:31:32 +1000

    Editing comments.

--- js/cite/cite.js
+++ js/cite/cite.js
@@ -1,53 +1,69 @@
 class Cite {
 
+	/***
+	 *** Cite class manages substring metadata.
+	 *
+	 *
+	 * Consumes content from document text node.
+	 * Separates substring data into segmented attributes.
+	 * Currencies class integrates subclass during text nodes matches operation.
+	 * Engineered as immutable object constructor.
+	 *
+	 */
+
 	constructor (textValue, index, textNodeSubstrings, textNodeValue) {
-		/** @param: @textValue, @type: document.textElement. **/
-		/** @param: @index: @type: @number. **/
-		/** @param: @textNodeSubstrings, @type: @array. **/
-		/** @param: @textNodeValue, @type: document.textElement. **/
-		
+		/* set class immutable properties. */
 		Object.defineProperties(this, {
 			textValue: {
+				/* contains base substring from textNodeValue. */
 				enumerable: true,
 				writable: false,
 				value: textValue
 			},
 			textValueLength: {
+				/* substring text size. */
 				enumerable: true,
 				writable: false,
 				value: textValue.length
 			},
 			textStartAt: {
+				/* substring starting index in textNodeValue. */
 				enumerable: true,
 				writable: false,
 				value: textNodeValue.indexOf(textValue)
 			},
 			textEndAt: {
+				/* substring ending index in textNodeValue. */
 				enumerable: true,
 				writable: false,
 				value: (textNodeValue.indexOf(textValue) + textValue.length)
 			},
 			textValueConsumes: {
+				/* percentage characters of substring compose main string. */
 				enumerable: true,
 				writable: false,
-				value: (textNodeValue.length - (textNodeValue.indexOf(textValue) + textValue.length))
+				value: ((textValue.length / textNodeValue.length) * 100)
 			},
 			textNodeSubstringPosition: {
+				/* position of substring in split substring array. */
 				enumerable: true,
 				writable: false,
 				value: index
 			},
 			textNodeSubstringSize: {
+				/* number of substrings for textNodeValue. */
 				enumerable: true,
 				writable: false,
 				value: textNodeSubstrings.length
 			},
 			textNodeValue: {
+				/* main string from which substrings were derrived. */
 				enumerable: true,
 				writable: false,
 				value: textNodeValue
 			},
 			textNodeValueLength: {
+				/* size of main text string. */
 				enumerable: true,
 				writable: false,
 				value: textNodeValue.length
